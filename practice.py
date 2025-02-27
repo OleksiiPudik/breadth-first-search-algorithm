ef04df86091259726013182c06e005ba3e174e9b
OleksiiPudik <alekseypudik@gmail.com>
2025-02-27 21:47:26 +0200

    First commit

--- practice.py
+++ practice.py
@@ -1,34 +1,52 @@
 graph = {}
+graph["start"] = {}
+graph["start"]["a"] = 6
+graph["start"]["b"] = 2
+graph["a"] = {}
+graph["a"]["fin"] = 1
+graph["b"] = {}
+graph["b"]["a"] = 3
+graph["b"]["c"] = 1
+graph["b"]["fin"] = 5
+graph["c"] = {}
+graph["c"]["fin"] = 1
+graph["fin"] = {}
 
-graph["you"] = ["alice", "bob", "clare"]
-graph["alice"] = ["peggi"]
-graph["bob"] = ["anuj", "peggi"]
-graph["clare"] = ["tom", "jonny"]
-graph["peggi"] = []
-graph["anuj"] = []
-graph["peggi"] = []
-graph["tom"] = []
-graph["jonny"] = []
+infinity = float("inf")
+costs = {}
+costs["a"] = 6
+costs["b"] = 2
+costs["c"] = infinity
+costs["fin"] = infinity
 
-from collections import deque
+parents = {}
+parents["a"] = "start"
+parents["b"] = "start"
+parents["fin"] = None
 
-def search(name):
-    search_queue = deque()
-    search_queue += graph[name]
-    searched = set()
+processed = []
 
-    def person_is_seller(name):
-        return name[-1] == "m"
+def find_lowest_cost_node(costs):
+   lowest_cost = float("inf")
+   lowest_cost_node = None
+   for node in costs:
+      cost = costs[node]
+      if cost < lowest_cost and node not in processed:
+         lowest_cost = cost
+         lowest_cost_node = node
+   return lowest_cost_node
 
-    while search_queue:
-        person = search_queue.popleft()
-        if person not in searched:
-            if person_is_seller(person):
-                print(person, " is a mango seller!")
-                return True
-            else:
-                search_queue += graph[person]
-                searched.add(person)
-    return False
+node = find_lowest_cost_node(costs)
+while node is not None:
+   cost = costs[node]
+   neighbors = graph[node]
+   for n in neighbors.keys():
+      new_cost = cost + neighbors[n]
+      if costs[n] > new_cost:
+         costs[n] = new_cost
+         parents[n] = node
+   processed.append(node)
+   node = find_lowest_cost_node(costs)
 
-search("you")
+
+print(costs["fin"])
